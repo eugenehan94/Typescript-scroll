578156691f8a9f40d0883b2a2b9a53ef7ea4afee
eugenehan94 <eugene.han94@gmail.com>
2022-06-28 18:28:17 -0400

    added html css and typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,86 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
 
-import confetti from 'canvas-confetti';
+// ********** set date ************
+// select span
+const date = document.getElementById("date") as HTMLTimeElement;
+const currentYear: number = new Date().getFullYear();
+const currentYearString: string = currentYear.toString()
+date!.innerHTML = currentYearString;
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// ********** close links ************
+const navToggle = document.querySelector<HTMLButtonElement>(".nav-toggle");
+const linksContainer = document.querySelector<HTMLElement>(".links-container");
+const links = document.querySelector<HTMLUListElement>(".links");
+
+navToggle?.addEventListener("click", () => {
+
+  const linksHeight: number | undefined = links?.getBoundingClientRect().height;
+  const containerHeight: number | undefined = linksContainer?.getBoundingClientRect().height;
+  if (containerHeight === 0) {
+    linksContainer!.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer!.style.height = "0";
+  }
+
+});
+
+// ********** fixed navbar ************
+
+const navbar = document.getElementById("nav") as HTMLElement;
+const topLink = document.querySelector<HTMLAnchorElement>(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight: number = window.pageYOffset;
+  const navHeight: number = navbar!.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar?.classList.add("fixed-nav");
+  } else {
+    navbar?.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+
+    topLink?.classList.add("show-link");
+  } else {
+    topLink?.classList.remove("show-link");
+  }
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll<HTMLAnchorElement>(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const target = e.currentTarget as HTMLAnchorElement
+    const id = target!.getAttribute("href")!.slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar!.getBoundingClientRect().height;
+    const containerHeight = linksContainer!.getBoundingClientRect().height;
+    const fixedNav = navbar?.classList.contains("fixed-nav");
+    let position = element!.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    linksContainer!.style.height = "0";
+  });
+});
+// calculate heights
+export {}
